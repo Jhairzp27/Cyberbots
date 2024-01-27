2ab41d643c0692d2ae98bf1cf7f45feb749107ac
Jhairzp_27 <jhairzp27@hotmail.com>
2024-01-27 14:29:34 -0500

    Add mechatronic, Interface skills, and person abstract class git status

---
+++ CyberBot/src/GZMecatronico.java
@@ -0,0 +1,5 @@
+public class GZMecatronico extends GZPersona{
+
+    @Override
+    public void gzRealizarActividad() {System.out.println("Creando exobot...");    }
+}
---
+++ CyberBot/src/GZPersona.java
@@ -0,0 +1,14 @@
+public abstract class GZPersona implements IGZHabilidades{
+    private String gzHabilidades;
+
+    public GZPersona() {
+    }
+
+    public String getGzHabilidades() {
+        return gzHabilidades;
+    }
+
+    public void setGzHabilidades(String gzHabilidades) {
+        this.gzHabilidades = gzHabilidades;
+    }
+}
