637a75bee9fa3e160de5455388dd83c3183d148b
Angel Pastaz <angel.pastaz@epn.edu.ec>
2024-01-28 22:29:42 -0500

    Creacion brazos

---
+++ CyberBot/src/APBrazoIzq.java
@@ -0,0 +1,9 @@
+public class APBrazoIzq extends APBrazo{
+
+    public APBrazoIzq(String brazo, String arma) {
+        super(brazo, arma);
+        //TODO Auto-generated constructor stub
+    }
+    
+
+}
