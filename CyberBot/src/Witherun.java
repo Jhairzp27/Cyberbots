578ab501e351aad89068d5a7b86a6c0daa182c76
Jhairzp_27 <jhairzp27@hotmail.com>
2024-01-30 00:28:22 -0500

    Add a screen cleaner

--- CyberBot/src/Witherun.java
+++ CyberBot/src/Witherun.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Witherun {
 
         IPColors color = new IPColors();
@@ -29,10 +31,13 @@ public class Witherun {
                 }
         }
 
-        public void ipCrear() {
+        public void ipCrear() throws Exception {
+                Scanner Scanner = new Scanner(System.in);
 
                 System.out.println(color.RED + "\n\n\t\t\t ------- WITHERUN --------- \n\n" + color.RESET + color.GREEN
                                 + ipUbicacion + color.RESET);
+                gzEsperarTecla(Scanner);
+                gzLimpiarConsola();
 
                 // Instancias
                 GZMecatronico mecatronico = new GZMecatronico();
@@ -41,22 +46,26 @@ public class Witherun {
                 SSExpertoIngles oSsExpertoIngles = new SSExpertoIngles();
                 IPExoBot exoBot = new IPExoBot();
 
-        // Creacion de exobot mediante el codigo de activacion
-        mecatronico.crearExobot(oExoBot);
-        mecatronico.personalizarExobot();
-        exoBot.cpActivarFuentePoder();
-        // SSExpertoEspanol ssExpertoEspanol= new SSExpertoEspanol();
-        // SSExpertoIngles ssExpertoIngles= new SSExpertoIngles();
-        System.out.println(
-                "\u001B[34m"
-                        + "Para un mejor desenvolvimiento en la batalla, el IAbot del exobot aprenderá los idiomas básicos");
-        oSsExpertoEspanol.ssEnsenarIdioma(oExoBot);
-        oSsExpertoIngles.ssEnsenarIdioma(oExoBot);
-        oExoBot.ssAprenderIdiomaEspanol();
-        oExoBot.ssAprenderIdiomaIngles();
-        oExoBot.ssAprenderIdiomaIngles();
-        oExoBot.ssMostrarIdiomasAprendidos();
-        System.out.println("\u001B[0m");
+                // Creacion de exobot mediante el codigo de activacion
+                mecatronico.crearExobot(oExoBot);
+                mecatronico.personalizarExobot();
+                exoBot.cpActivarFuentePoder();
+                gzEsperarTecla(Scanner);
+                gzLimpiarConsola();
+                // SSExpertoEspanol ssExpertoEspanol= new SSExpertoEspanol();
+                // SSExpertoIngles ssExpertoIngles= new SSExpertoIngles();
+                System.out.println(
+                                "\u001B[34m"
+                                                + "Para un mejor desenvolvimiento en la batalla, el IAbot del exobot aprenderá los idiomas básicos");
+                oSsExpertoEspanol.ssEnsenarIdioma(oExoBot);
+                oSsExpertoIngles.ssEnsenarIdioma(oExoBot);
+                oExoBot.ssAprenderIdiomaEspanol();
+                oExoBot.ssAprenderIdiomaIngles();
+                oExoBot.ssAprenderIdiomaIngles();
+                oExoBot.ssMostrarIdiomasAprendidos();
+                System.out.println("\u001B[0m");
+                gzEsperarTecla(Scanner);
+                gzLimpiarConsola();
 
                 // Metodos de ExoBot - Isaac Proaño
                 IPpresentarLoading("Creando piernas");
@@ -66,17 +75,40 @@ public class Witherun {
                 exoBot.IPSaltar();
                 exoBot.IPSacrificarPiernaIzquierda();
                 exoBot.IPSacrificarPiernaDerecha();
+                gzEsperarTecla(Scanner);
+                gzLimpiarConsola();
 
                 // Metodos de brazos - armas
                 IPpresentarLoading("Creando brazos");
                 System.out.println(" ");
                 exoBot.pjAsignarArma();
                 exoBot.srDispararArmaDerecho();
+                gzEsperarTecla(Scanner);
+                gzLimpiarConsola();
 
-        // Metodos de fuente de poder
-        exoBot.cpCargarEnergia(100);
-        exoBot.cpConsumirEnergia(50);
-        exoBot.cpDesactivarFuentePoder();
-    }
+                // Metodos de fuente de poder
+                exoBot.cpCargarEnergia(100);
+                exoBot.cpConsumirEnergia(50);
+                exoBot.cpDesactivarFuentePoder();
+        }
+
+        /**
+         * Este metodo limpia la consola luego de utilizar el scanner
+         * @author Jhair Zambrano
+         * @throws Exception
+         */
+        private static void gzLimpiarConsola() throws Exception {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        }
+        
+        /**
+         * Este metodo espera a que escribas para luego darle instrucciones
+         * @author Jhair Zambrano
+         * @param sc
+         */
+        private static void gzEsperarTecla(Scanner sc) {
+                System.out.print(IPColors.CYAN+"\nPresione cualquier tecla para continuar..." + IPColors.RESET);
+                sc.nextLine();
+        }
 
 }
